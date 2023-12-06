ec8a4807f48b1f58e676b3fac4f2b34623ab6121
Yusuf Kaplan <yusufkaplan3591@gmail.com>
2023-12-06 23:21:43 +0300

    Day6

--- src/main/java/com/project/security/jwt/JwtUtils.java
+++ src/main/java/com/project/security/jwt/JwtUtils.java
@@ -1,8 +1,67 @@
 package com.project.security.jwt;
 
+import com.project.security.service.UserDetailsImpl;
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class JwtUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${backendapi.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${backendapi.app.jwtExpirationMs}")
+    private long jwtExpirationMs;
+
+    //Not: Generate JWT
+    public String generateJwtToken(Authentication authentication){
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        return generateJwtTokenFromUsername(userDetails.getUsername());
+    }
+
+    public String generateJwtTokenFromUsername(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    // Not: Validate JWT
+    public boolean validateJwtToken(String jwtToken){
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwtToken);
+            return true;
+        } catch (ExpiredJwtException e) {
+            LOGGER.error("Jwt token is expired : {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            LOGGER.error("Jwt token is unsupported : {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            LOGGER.error("Jwt token is invalid : {}", e.getMessage());
+        } catch (SignatureException e) {
+            LOGGER.error("Jwt Signature is invalid : {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("Jwt is empty : {}", e.getMessage());
+        }
+        return false;
+    }
+
+    // Not: getUsernameFromJWT
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
 
 }
